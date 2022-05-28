18625062623f1268599bb670ff72a0278971512b
maeda577 <kazu55632000@gmail.com>
2022-05-28 17:31:28 +0900

    定期cron更新時にstdoutを使わないようにした

--- scripts/miracron-update.sh
+++ scripts/miracron-update.sh
@@ -1,7 +1,9 @@
 #!/bin/ash
 set -eu
 
-unset MIRACRON_RULES
-MIRACRON_RULES="$(python3 /usr/local/lib/miracron/miracron.py)"
-
-(echo "${MIRACRON_UPDATE_SCHEDULE:-0 5 * * *} miracron-update.sh"; echo "$MIRACRON_RULES") | busybox crontab -
+# 録画ルール生成(ファイルは上書きされる)
+python3 /usr/local/lib/miracron/miracron.py --outfile /var/spool/cron/crontabs/root
+# 定期アップデートルールを末尾に追加
+echo "${MIRACRON_UPDATE_SCHEDULE:-0 5 * * *} miracron-update.sh" >> /var/spool/cron/crontabs/root
+# cron更新したフラグを作成
+echo 'root' >> /var/spool/cron/crontabs/cron.update

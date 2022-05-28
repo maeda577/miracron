cd43161a83f99195abc519eb5b5bee5d60a53da3
maeda577 <kazu55632000@gmail.com>
2022-05-28 17:55:20 +0900

    cronルールのコピーを作るようにした

--- scripts/miracron-update.sh
+++ scripts/miracron-update.sh
@@ -7,3 +7,8 @@ python3 /usr/local/lib/miracron/miracron.py --outfile /var/spool/cron/crontabs/r
 echo "${MIRACRON_UPDATE_SCHEDULE:-0 5 * * *} miracron-update.sh" >> /var/spool/cron/crontabs/root
 # cron更新したフラグを作成
 echo 'root' >> /var/spool/cron/crontabs/cron.update
+
+# cronルールを閲覧するためだけのコピーを作る
+if [ -n "${MIRACRON_CRON_COPY-}" ]; then
+    cp /var/spool/cron/crontabs/root "${MIRACRON_CRON_COPY}"
+fi

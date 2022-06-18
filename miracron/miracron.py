e85d0a21e1534e3f445342a3fb30400e6f8348cb
maeda577 <kazu55632000@gmail.com>
2022-06-18 19:52:14 +0900

    Program型を定義

--- miracron/miracron.py
+++ miracron/miracron.py
@@ -1,4 +1,5 @@
 import argparse
+import dataclasses
 import datetime
 import json
 import logging
@@ -22,16 +23,68 @@ FILENAME_TRANS_MAP: dict[str, str] = {
     '#': '_',
 }
 
+@dataclasses.dataclass(frozen=True)
+class Video:
+    type: str
+    resolution: str
+    streamContent: int
+    componentType: int
+
+@dataclasses.dataclass(frozen=True)
+class Audio:
+    componentType: int
+    isMain: bool
+    samplingRate: int
+    langs: list[str]
+
+@dataclasses.dataclass(frozen=True)
+class Genre:
+    lv1: int
+    lv2: int
+    un1: int
+    un2: int
+
+# 番組情報
+@dataclasses.dataclass(frozen=True)
+class Program:
+    id: int
+    eventId: int
+    serviceId: int
+    transportStreamId: int
+    networkId: int
+    startAt: int
+    duration: int
+    isFree: bool
+    name: str
+    audio: Audio
+    audios: list[Audio]
+    description: str = None
+    video: Video = None
+    extended: dict[str, str] = dataclasses.field(default_factory=dict[str, str])
+    genres: list[Genre] = dataclasses.field(default_factory=list[Genre])
+
+def _cast_json(dict: dict[str, any]) -> any:
+    if 'un1' in dict:
+        return Genre(**dict)
+    if 'samplingRate' in dict:
+        return Audio(**dict)
+    if 'resolution' in dict:
+        return Video(**dict)
+    if 'name' in dict:
+        return Program(**dict)
+
+    return dict
+
 # 番組が検索条件にマッチするか
-def _is_match_config(program, config) -> bool:
+def _is_match_config(program: Program, config) -> bool:
     # 単発録画IDにマッチすれば他のルールを考慮せずTrue
-    if 'oneshots' in config.keys() and program['id'] in config['oneshots']:
+    if 'oneshots' in config.keys() and program.id in config['oneshots']:
         return True
     # ルールの判定
     if 'rules' in config.keys():
         for rule in config['rules']:
-            # サービスIDが指定されていれば判定(プログラム側にServiceIDが無ければ即False)
-            if 'serviceIds' in rule.keys() and ('serviceId' not in program.keys() or program['serviceId'] not in rule['serviceIds']):
+            # サービスIDが指定されていれば判定
+            if 'serviceIds' in rule.keys() and (program.serviceId not in rule['serviceIds']):
                 continue
             # 文字列系の判定
             if ('keywords' in rule.keys() or 'excludeKeywords' in rule.keys()) and _is_match_string(program, rule):
@@ -39,29 +92,31 @@ def _is_match_config(program, config) -> bool:
     return False
 
 # 番組が検索条件にマッチするか（文字列関連のみ）
-def _is_match_string(program, rule) -> bool:
-    target_string = []
-    if rule['matchName'] and 'name' in program.keys():
-        target_string.append(program['name'])
-    if rule['matchDescription'] and 'description' in program.keys():
-        target_string.append(program['description'])
-    if rule['matchExtended'] and 'extended' in program.keys():
-        target_string.extend(program['extended'].values())
+def _is_match_string(program: Program, rule) -> bool:
+    target_string: list[str] = []
+    if rule['matchName']:
+        target_string.append(program.name)
+    if rule['matchDescription']:
+        target_string.append(program.description)
+    if rule['matchExtended']:
+        target_string.extend(program.extended.keys())
+        target_string.extend(program.extended.values())
 
+    target_string = list(filter(lambda s: s != None, target_string))
     if len(target_string) == 0:
         return False
 
     if 'excludeKeywords' in rule.keys():
         for exclude_keyword in rule['excludeKeywords']:
-            for str in target_string:
-                if exclude_keyword in str:
+            for string in target_string:
+                if exclude_keyword in string:
                     return False
 
     if 'keywords' in rule.keys():
         for keyword in rule['keywords']:
             has_keyword = False
-            for str in target_string:
-                if keyword in str:
+            for string in target_string:
+                if keyword in string:
                     has_keyword = True
                     break
             if has_keyword == False:
@@ -152,18 +207,19 @@ if __name__ == '__main__':
     logger.debug('Loading configuration is completed. Start getting the programs.')
 
     # 番組表の取得
-    programs_url = urllib.parse.urljoin(config['mirakurunUrl'], 'api/programs')
-    req = urllib.request.Request(programs_url)
+    programs_url: str = urllib.parse.urljoin(config['mirakurunUrl'], 'api/programs')
+    req: urllib.request.Request = urllib.request.Request(programs_url)
     try:
         with urllib.request.urlopen(req) as res:
-            programs: list[any] = json.load(res)
+            programs: list[Program] = json.load(res, object_hook=_cast_json)
+            filtered_programs = list(filter(lambda p: type(p) == Program, programs))
     except Exception as e:
         logger.error('Failed to get programs')
         logger.exception(e)
         sys.exit(1)
 
     # ルールにマッチするものに絞り込んで日付順に並び替え
-    match_programs = sorted(filter(lambda p:_is_match_config(p, config), programs), key=lambda p: p['startAt'])
+    match_programs = sorted(filter(lambda p:_is_match_config(p, config), filtered_programs), key=lambda p: p.startAt)
 
     logger.debug('Getting programs and filtering is completed. Start generating cron rules.')
 
@@ -174,20 +230,20 @@ if __name__ == '__main__':
     cron_list = []
     # cronルールの生成
     for program in match_programs:
-        start_at = datetime.datetime.fromtimestamp(program['startAt']/1000, timezone)
+        start_at = datetime.datetime.fromtimestamp(program.startAt / 1000, timezone)
         start_margin = start_at - margin_sec
-        info_url = urllib.parse.urljoin(config['mirakurunUrl'], f"api/programs/{program['id']}")
-        stream_url = urllib.parse.urljoin(config['mirakurunUrl'], f"api/programs/{program['id']}/stream")
+        info_url = urllib.parse.urljoin(config['mirakurunUrl'], f"api/programs/{program.id}")
+        stream_url = urllib.parse.urljoin(config['mirakurunUrl'], f"api/programs/{program.id}/stream")
 
         # 出力先などの準備
-        dir_name = start_at.strftime("%Y%m%d") + "_" + (program['name'].translate(translate_map) if 'name' in program.keys() else '')
+        dir_name = start_at.strftime("%Y%m%d") + "_" + (program.name.translate(translate_map) if program.name != None else '')
         dir_path = os.path.join(config['recordDirectory'], dir_name)
-        stream_path = os.path.join(dir_path, str(program['id']) + '.m2ts')
-        info_path = os.path.join(dir_path, str(program['id']) + '.json')
-        log_path = os.path.join(dir_path, str(program['id']) + '.log')
+        stream_path = os.path.join(dir_path, str(program.id) + '.m2ts')
+        info_path = os.path.join(dir_path, str(program.id) + '.json')
+        log_path = os.path.join(dir_path, str(program.id) + '.log')
 
         # cron文字列
-        comment_str = f"# ID:{program['id']} ServiceID: {program['serviceId']} StartAt: {start_at} {dir_name}"
+        comment_str = f"# ID:{program.id} ServiceID: {program.serviceId} StartAt: {start_at} {dir_name}"
         cron_str = f"{start_margin.minute} {start_margin.hour} {start_margin.day} {start_margin.month} * " \
             f"sleep {start_margin.second} && " \
             f"mkdir -p '{dir_path}' && " \
